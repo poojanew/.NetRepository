fadecc4fb08f9dc36b793a236a38675e20ba7123
XOR-IND\newase_p <newasepooja95@gmail.com>
2017-09-06 16:39:03 +0530

    Assignment on Application Security, Permission

---
+++ AuthenticationDemo1/Controllers/SecretController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuthenticationDemo1.Controllers
+{
+    [Authorize]
+    public class SecretController : Controller
+    {
+        public ContentResult Secret()
+        {
+            return Content("Secret informations here");
+        }
+        [AllowAnonymous]
+        public ContentResult PublicInfo()
+        {
+            return Content("Public informations here");
+        }
+
+    }
+}
